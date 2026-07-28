d4e74d7df0ba357395bfd8d6bd2be305648910d9
rameshworkatuwal <riteshkatwal25@gmail.com>
2026-07-28 20:59:56 +0545

    Add files via upload

--- site.js
+++ site.js
@@ -16,6 +16,11 @@
     if (portfolioLink) {
       portfolioLink.href = 'portfolio.html';
       portfolioLink.textContent = 'Portfolio';
+
+      var certificationsLink = links.querySelector('a[href="certifications.html"]');
+      if (certificationsLink && certificationsLink.parentElement && portfolioLink.parentElement) {
+        links.insertBefore(portfolioLink.parentElement, certificationsLink.parentElement);
+      }
     }
 
     var toggle = nav.querySelector('.nav-toggle');
